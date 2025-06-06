976865f7363906d5ff60cfd0c3d406b34c780760
malai <vinodmalai94@gmail.com>
2025-06-06 13:37:35 +0530

    Hello

---
+++ src/main/java/com/interfaceelements/MenOrderPageInterFaceElement.java
@@ -0,0 +1,11 @@
+package com.interfaceelements;
+
+public interface MenOrderPageInterFaceElement {
+	
+	String PlaceOrder_xpath ="//button[@class='action primary checkout']";
+	String thankyouMsg_xpath="//*[text()='Thank you for your purchase!']";
+	String orderId_xpath="//a[@class='order-number']";
+	
+}
+
+
